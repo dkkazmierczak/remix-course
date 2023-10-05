227b5fa9fb4561e36b16cd90a9982a3eb0dc5259
Dominika Kazmierczak <dominika@enabler.no>
2023-10-05 10:56:06 +0200

    Error handling

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -1,5 +1,5 @@
 import type { LinksFunction } from '@remix-run/node';
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { Link, Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 
 import styles from './styles/main.css';
 import MainNavigation from './components/MainNavigation';
@@ -28,6 +28,33 @@ export default function App() {
   );
 }
 
-// export function links() {
-//   return [{rel: "stylesheet", href: cssBundleHref}]
-// }
+//this can be added in any page to show a custom error message
+// It is shown istead of the content of that file
+export function ErrorBoundary({ error }: any) {
+  return (
+    <html lang='en'>
+      <head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width,initial-scale=1' />
+        <Meta />
+        <Links />
+        <title>An error occured</title>
+      </head>
+      <body>
+        <header>
+          <MainNavigation />
+        </header>
+        <main className='error'>
+          <h1>An error occured!</h1>
+          <p>{error.message}</p>
+          <p>
+            Back to <Link to='/'>safety</Link>!
+          </p>
+        </main>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
